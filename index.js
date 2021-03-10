bec62723832456fd6a1ba5a0e7ce59407f8473e0
Diganta Som <itsdsom111@gmail.com>
2021-03-10 10:14:14 +0530

    Set up database & server

---
+++ index.js
@@ -0,0 +1,46 @@
+const { ApolloServer, gql } = require('apollo-server'); // using express bts automatically
+const mongoose = require('mongoose');
+
+const { MONGODB_URI } = require('./config');
+const Post = require('./models/Post');
+
+const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
+
+  type Query {
+    getPosts: [Post]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    async getPosts() {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('MongoDB connected...');
+    return server.listen({ port: 5000 });
+  })
+  .then(res => {
+    console.log(`Server running at ${res.url}`);
+  });
